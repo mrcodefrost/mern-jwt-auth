55fbe4ab92802e3267c4eb1d0d0ff7eb2dcb0523
mrcodefrost <mr.codefrost@gmail.com>
2025-04-29 18:32:12 +0530

    Fixed error handler type mismatch

--- backend/src/middleware/errorHandler.ts
+++ backend/src/middleware/errorHandler.ts
@@ -2,7 +2,8 @@ import { ErrorRequestHandler, NextFunction, Request, Response } from "express";
 
 const errorHandler: ErrorRequestHandler = (error, req: Request, res:Response, next: NextFunction) => {
     console.log(`PATH: ${req.path}`, error);
-    return res.status(500).send("Internal server error");
+    res.status(500).send("Internal server error");
+    // Adding return keyword on res.status will throw error on express 5
 }
 
 export default errorHandler;    
