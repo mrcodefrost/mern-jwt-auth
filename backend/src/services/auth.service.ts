0490851ad165149a45fa3b968fc3b23e1aefbcae
mrcodefrost <mr.codefrost@gmail.com>
2025-05-01 18:44:18 +0530

    Refresh and session added in service

--- backend/src/services/auth.service.ts
+++ backend/src/services/auth.service.ts
@@ -1,4 +1,7 @@
+import jwt from "jsonwebtoken";
+import { JWT_REFRESH_SECRET, JWT_SECRET } from "../constants/env";
 import VerificationCodeType from "../constants/verificationCodeTypes";
+import SessionModel from "../models/session.model";
 import UserModel from "../models/user.model";
 import VerificationCodeModel from "../models/verificationCode.model";
 import { oneYearFromNow } from "../utils/date";
@@ -37,7 +40,38 @@ export const createAccount = async(data: CreateAccountParams) => {
      })
 
      // send verification email 
+
+
      // create session 
+     const session = await SessionModel.create({
+      userId: user._id,
+      userAgent: data.userAgent,
+     });
+
+
      // sign access token & refresh token
+     const refreshToken = jwt.sign(
+      {sessionId: session._id},
+      JWT_REFRESH_SECRET, {
+         audience: ["user"],
+         expiresIn: "30d",
+      },
+     );
+
+     const accessToken = jwt.sign(
+      {
+         userId: user._id,
+         sessionId: session._id},
+      JWT_SECRET, {
+         audience: ["user"],
+         expiresIn: "15m",
+      },
+     );
+
      // return user and tokens
+     return {
+      user, 
+      accessToken, 
+      refreshToken,
+     };
 }
